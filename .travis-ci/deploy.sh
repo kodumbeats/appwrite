0e24d8b79d837b70780833b6cbba0ad78ece56b9
kodumbeats <brandon.leckemby@gmail.com>
2021-11-08 20:23:49 -0500

    TEST COMMIT
    TEST COMMIT

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -1,1 +1,11 @@
-echo 'Nothing to deploy right now.'
+#!/bin/bash
+
+# $TRAVIS_BRANCH:
+#   - for push builds, or builds not triggered by a pull request,
+#     this is the name of the branch.
+#   - for builds triggered by a pull request this is the name
+#     of the branch targeted by the pull request.
+#   - for builds triggered by a tag, this is the same as the name
+#     of the tag (TRAVIS_TAG)
+
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7,linux/ppc64le,linux/s390x -t kodumbeats/appwrite:$TRAVIS_BRANCH -t kodumbeats/appwrite:latest ./ --push
