703e296baf6d539c1ef7c1c6aaf9eabf54e57544
kodumbeats <brandon.leckemby@gmail.com>
2021-11-09 08:37:25 -0500

    TEST COMMIT

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -8,4 +8,5 @@
 #   - for builds triggered by a tag, this is the same as the name
 #     of the tag (TRAVIS_TAG)
 
-docker buildx build --build-arg VERSION=$TRAVIS_BRANCH --platform linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7,linux/ppc64le,linux/s390x -t kodumbeats/appwrite:$TRAVIS_BRANCH -t kodumbeats/appwrite:latest ./ --push
+# docker buildx build --build-arg VERSION=$TRAVIS_BRANCH --platform linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7,linux/ppc64le,linux/s390x -t kodumbeats/appwrite:$TRAVIS_BRANCH -t kodumbeats/appwrite:latest ./ --push
+docker buildx build --build-arg VERSION=$TRAVIS_BRANCH --platform linux/amd64,linux/arm64 -t kodumbeats/appwrite:$TRAVIS_BRANCH -t kodumbeats/appwrite:latest ./ --push
